7b18982079a05028bb82dd883b9f5cfe143a649e
V-YOP <V-YOP@pm.me>
2022-06-17 17:00:05 +0800

    画饼

--- src/main/java/me/yuuki/todoapp/dao/TaskDao.java
+++ src/main/java/me/yuuki/todoapp/dao/TaskDao.java
@@ -1,20 +1,45 @@
 package me.yuuki.todoapp.dao;
 
+import me.yuuki.todoapp.entity.TaskEntity;
 import me.yuuki.todoapp.model.Task;
 
+import java.util.Date;
 import java.util.List;
 
+/**
+ * Task 的 DAO，分页等操作当前不考虑。
+ */
 public interface TaskDao {
 
-    List<Task> selectAllTask(String userId);
+    TaskEntity select(long taskId);
 
-    List<Task> selectWorkingTask(String userId);
+    List<TaskEntity> selectAllTask(String userId);
 
-    List<Task> selectDoneTask(String userId);
+    /**
+     * 返回所有未完成的 Task
+     */
+    List<TaskEntity> selectWorkingTask(String userId);
 
-    boolean addTask(String userId, Task task);
+    /**
+     * 返回所有未完成且当前日期在起始日期和终止日期之间的 Task
+     */
+    List<TaskEntity> selectValidTask(String userId);
 
-    boolean deleteTask(String userId, long taskId);
+    /**
+     * 返回所有未完成且当前日期不在起始日期和终止日期之间的 Task
+     */
+    List<TaskEntity> selectOutDatedTask(String userId);
 
-    void doneTask(String userId, long taskId);
+    /**
+     * 返回特定日期的 valid 的 Task
+     */
+    List<TaskEntity> selectValidTask(String userId, Date date);
+
+    List<TaskEntity> selectDoneTask(String userId);
+
+    long addTask(TaskEntity task);
+
+    boolean deleteTask(long taskId);
+
+    void doneTask(long taskId);
 }
---
+++ src/main/java/me/yuuki/todoapp/entity/User.java
@@ -0,0 +1,4 @@
+package me.yuuki.todoapp.entity;
+
+public class User {
+}
